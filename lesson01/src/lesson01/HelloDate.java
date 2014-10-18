cd0831d99c5d339e9ff23b2e84c0c06d5397909b
UserJava <UserJava@office5>
2014-10-18 17:44:32 +0300

    HelloDate class modified

--- lesson01/src/lesson01/HelloDate.java
+++ lesson01/src/lesson01/HelloDate.java
@@ -3,7 +3,7 @@ package lesson01;
 public class HelloDate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java 8!");
+		System.out.println("Hello from Java 8 Modified!");
 
 	}
 
