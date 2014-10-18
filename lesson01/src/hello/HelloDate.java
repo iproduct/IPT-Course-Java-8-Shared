17b931afebc4bf120a139ecacd20b6c6bbedd119
Trayan Iliev <office@iproduct.org>
2014-10-18 23:58:09 +0200

    Update HelloDate.java

--- lesson01/src/hello/HelloDate.java
+++ lesson01/src/hello/HelloDate.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloDate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java 8 with Lambdas!");
+		System.out.println("Hello from Java 8 with lambda expressions!");
 		Helper helper = new Helper();
 		System.out.println(helper.getMessage());
 	}
