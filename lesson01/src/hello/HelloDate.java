bdffc7add2ebbbb665f411255f9b968004730f10
Trayan Iliev <office@iproduct.org>
2014-10-18 23:17:51 +0200

    Update HelloDate.java

--- lesson01/src/hello/HelloDate.java
+++ lesson01/src/hello/HelloDate.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloDate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java 8 Modified!");
+		System.out.println("Hello from Java 8!");
 		Helper helper = new Helper();
 		System.out.println(helper.getMessage());
 	}
