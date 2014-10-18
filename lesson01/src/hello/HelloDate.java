f7babc52ea6f0b8c3df5e6e56c8d05a9c6992d8b
iproduct <office@iproduct.org>
2014-10-19 00:18:08 +0200

    Message changed

--- lesson01/src/hello/HelloDate.java
+++ lesson01/src/hello/HelloDate.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloDate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java 8!");
+		System.out.println("Hello from Java 8 M2!");
 		Helper helper = new Helper();
 		System.out.println(helper.getMessage());
 	}
