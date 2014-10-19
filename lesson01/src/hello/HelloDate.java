f4147ad051c0a157e6c40220b6d35843a93ac018
iproduct <office@iproduct.org>
2014-10-19 10:52:04 +0200

    Message changed

--- lesson01/src/hello/HelloDate.java
+++ lesson01/src/hello/HelloDate.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloDate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java 8 with lambda expressions!");
+		System.out.println("Hello from Java 8 with lambdas!");
 		Helper helper = new Helper();
 		System.out.println(helper.getMessage());
 	}
