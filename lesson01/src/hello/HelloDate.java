160991e8a0c2b2011465b482174dd24fdf3737c3
iproduct <office@iproduct.org>
2014-10-19 10:29:27 +0200

    Base versoin submitted

--- lesson01/src/hello/HelloDate.java
+++ lesson01/src/hello/HelloDate.java
@@ -1,12 +1,9 @@
 package hello;
-
-import java.util.Date;
  
 public class HelloDate {
 
 	public static void main(String[] args) {
-		Date date = new Date();	
-		System.out.println(date + ": Hello from Java 8 with lambda expressions!");
+		System.out.println("Hello from Java 8 with lambda expressions!");
 		Helper helper = new Helper();
 		System.out.println(helper.getMessage());
 	}
